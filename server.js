95a483138e5c3c34d3723a28cf4e2b059402c0ea
Steve Denning <steve.denning@cgi.com>
2025-06-27 17:20:24 +0100

    update files to create book and author

--- server.js
+++ server.js
@@ -87,8 +87,49 @@ const RootQueryType = new GraphQLObjectType({
 	}),
 });
 
+const RouteMutationType = new GraphQLObjectType({
+	name: "Mutation",
+	description: "Root Mutation",
+	fields: () => ({
+		addBook: {
+			type: BookType,
+			description: "Add a book",
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+				authorId: { type: GraphQLNonNull(GraphQLInt) },
+			},
+			resolve: (parent, args) => {
+				const newBook = {
+					id: books.length + 1,
+					name: args.name,
+					authorId: args.authorId,
+				};
+				books.push(newBook);
+				return newBook;
+			},
+		},
+		addAuthor: {
+			type: AuthorType,
+			description: "Add an author",
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+				id: { type: GraphQLNonNull(GraphQLInt) },
+			},
+			resolve: (parent, args) => {
+				const newAuthor = {
+					id: authors.length + 1,
+					name: args.name,
+				};
+				authors.push(newAuthor);
+				return newAuthor;
+			},
+		},
+	}),
+});
+
 const schema = new GraphQLSchema({
 	query: RootQueryType,
+	mutation: RouteMutationType,
 });
 
 app.use(
